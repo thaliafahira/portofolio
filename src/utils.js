940764260e67344818f282f8806d470aa89d4862
leaxz <thaliafahira@gmail.com>
2025-02-03 18:43:04 +0700

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,3 @@
 export const getImageUrl = (path) => {
-    const baseUrl = new URL('/', window.location.href).href;
-    return new URL(`${baseUrl}assets/${path}`, window.location.href).href;
-  };
+  return `${import.meta.env.BASE_URL}assets/${path}`;
+};
